b627185c57ee6aed9f7c11dd5deef00794f27ce3
stevecoombs <steven.lynn.coombs@gmail.com>
2015-04-26 22:38:28 -0600

    three of four down

--- objectReview.js
+++ objectReview.js
@@ -81,15 +81,26 @@ console.log(user);
 
   //Your code here
 
-//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
+var methodCollection = {};
+
+//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' 
+//and another method called logHello which logs 'hello' to the console. 
 
   //Your code here
+
+methodCollection.alertHello = function() {
+  alert('hello');
+}
+methodCollection.logHello = function() {
+  console.log('hello');
+}
 
 //Now call your alertHello and logHello methods. 
 
   //Your code here
 
-
+methodCollection.alertHello();
+methodCollection.logHello();
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
